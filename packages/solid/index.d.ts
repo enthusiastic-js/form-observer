cccbf87d23e0181e1b76f0864afa85ed3c7583dc
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2023-10-27 11:44:10 -0400

    chore: Expose `defaultErrorRendererSolid` to Devs

--- packages/solid/index.d.ts
+++ packages/solid/index.d.ts
@@ -1,3 +1,4 @@
 export * from "@form-observer/core";
 export { default as createFormValidityObserver } from "./createFormValidityObserver.js";
+export * from "./createFormValidityObserver.js";
 export type * from "./types.d.ts";
--- packages/solid/index.js
+++ packages/solid/index.js
@@ -1,2 +1,3 @@
 export * from "@form-observer/core";
 export { default as createFormValidityObserver } from "./createFormValidityObserver.js";
+export * from "./createFormValidityObserver.js";
